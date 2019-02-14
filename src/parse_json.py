f86bda03e4ab4ae379f0dcd767c670ccf70be270
smandekar1 <smandekar1@yahoo.com>
2019-02-14 14:48:31 -0600

    added parse_json file

--- src/parse_json.py
+++ src/parse_json.py
@@ -7,41 +7,36 @@ from searchtweets import ResultStream, gen_rule_payload, load_credentials
 
 import json
 
-with open('tweets.json') as json_data:
+with open('tweets.json', 'r') as json_data:
     tweets = json.load(json_data)
-    # print(type(tweets))
-    # print(d)
-    # print(type(tweets['id']))
-    # for tweet in tweets:
-    # print(tweets['id'])
+    print(type(tweets))
+    # print(len(tweets['text']))
+    
+    tweets = tweets[0:100]
 
-for tweet in tweets[0:100]:
+    with open('only_text.json', mode='w', encoding='utf-8') as f:
+    	json.dump([], f)
+    	tweet2 = []
 
+    for tweet in tweets:
+    	tweet["text"] = tweet["text"].translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\\'\n'|\\`~-=_+"})
 
-	# for cow in tweet['retweeted_status']:
-	# # 	for status in ['retweeted_status']['favorite_count']:
-	# # 		status = int(status)
-	# 		print(cow['lang'])
-	# # 	print(str(cow['favorite_count']))
+    	# print(tweet['text'])
+    	entry = {"text": tweet["text"]}
 
+    	tweet2.append(entry)
+    	
 
-	# 	print(cow['source'])
-		# print(cow['favorite_count'])
-		# print(['retweeted_status'])
-	# print(tweet['retweeted_status':{'favorite_count'}])
-	# if 'retweeted_status' in tweet:
-	if tweet.get('retweeted_status'):
-		# print(tweet.get(['retweeted_status']['favorite_count']))
+with open('only_text.json', mode='w', encoding='utf-8') as feedsjson:
+	# entry = {"text": tweet["text"]}
+	# tweet2.append(entry)
+	json.dump(tweet2, feedsjson, indent=2)
 
-	# if 'retweeted_status' in tweet and 15 < tweet['retweeted_status']['favorite_count'] < 5000:
-		print(tweet['text'] + tweet['created_at']+ "favorite_count: " + str(tweet['retweeted_status']['favorite_count']) + "\n")
+	# print(tweet2)
 
-	# print(tweet['retweeted_status']['favorite_count']) - works
+	with open('tweets_test.json', 'w') as fp:
 
-	# print(tweet['user']['location']) - works
-
-	# print(tweet['id']) - works 	
+		json.dump(tweets, fp, indent=2)
+	print(type(tweets))
 
 
-
-#is a list going in.  
