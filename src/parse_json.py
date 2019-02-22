a1a805d7695f129d6030004f9396f31f628c7737
smandekar1 <smandekar1@yahoo.com>
2019-02-22 12:12:08 -0600

    added dockerfile

--- src/parse_json.py
+++ src/parse_json.py
@@ -1,8 +1,7 @@
 # import json
 from searchtweets import ResultStream, gen_rule_payload, load_credentials
-
-# with open('tweets.json', 'r') as outfile:
-# 	outfile.read(tweets)
+from collections import Counter
+from stop_words import get_stop_words
 
 
 import json
@@ -28,8 +27,7 @@ with open('tweets.json', 'r') as json_data:
     	
 
 with open('only_text.json', mode='w', encoding='utf-8') as feedsjson:
-	# entry = {"text": tweet["text"]}
-	# tweet2.append(entry)
+	
 	json.dump(tweet2, feedsjson, indent=2)
 
 	# print(tweet2)
@@ -39,4 +37,44 @@ with open('only_text.json', mode='w', encoding='utf-8') as feedsjson:
 		json.dump(tweets, fp, indent=2)
 	print(type(tweets))
 
+
+with open('only_text.json', mode='r', encoding='utf-8') as words_file:
+
+	d = []
+
+	for tweet in tweets:
+		entry = tweet["text"].split()
+		entry_lower = [x.lower() for x in entry]
+		d += entry_lower	
+		# d.append(entry)
+
+	# with open('stopwords.txt') as fp:
+	# 	stopwords = set(fp.readlines())
+	# 	stopwords = stopwords.strip()
+	# # stopwords = open('stopwords.txt').readlines()	
+	# # stopwords = set(line.strip() for line in open('stopwords.txt'))
+	stop_words = get_stop_words('english')
+	added_words = ['rt', 't','https', 'people', 'co', 'people', 'la', 'el']
+	stop_words = added_words + stop_words
+	print("stopwords \n------------------------------------------------------------------------------", stop_words)
+	
+	# stopwords = nltk.download('stopwords')
+	# # stopwords = stopwords.words('english')
+	# print(stopwords.words('english')[620:680])
+	# for word in d:
+	# 	if word.lower() in stopwords:
+	# 		del word
+	e = []
+	for word in d:
+		if word not in stop_words:
+			e.append(word)
+	
+
+	print(type(d))
+	print(type(stop_words))
+	# print(d)
+
+	counted = Counter(e).most_common(15)
+
+	print("--------------\n", counted)
 
---
+++ src/plot.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from parse_json import counted
+
+
+counts = []
+
+for key, value in counted:
+    counts.append(value)
+    # counts += value
+print("counts", counts)
+men_means = counts
+
+labels = []
+
+for key, value in counted:
+    labels.append(key)
+    # counts += value
+print("labels", labels)
+# men_means = counts
+
+# men_means = (20, 35, 30, 35, 27)
+# women_means, women_std = (25, 32, 34, 20, 25), (3, 5, 2, 3, 3)
+
+ind = np.arange(len(men_means))  # the x locations for the groups
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind, men_means, width,
+                color='SkyBlue', label='Count')
+# rects2 = ax.bar(ind + width/2, women_means, width, yerr=women_std,
+#                 color='IndianRed', label='Women')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores')
+ax.set_title('Most Common Words from Tweets')
+ax.set_xticks(ind)
+ax.set_xticklabels(labels)
+ax.legend()
+
+
+def autolabel(rects, xpos='center'):
+    """
+    Attach a text label above each bar in *rects*, displaying its height.
+
+    *xpos* indicates which side to place the text w.r.t. the center of
+    the bar. It can be one of the following {'center', 'right', 'left'}.
+    """
+
+    xpos = xpos.lower()  # normalize the case of the parameter
+    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+    offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()*offset[xpos], 1.01*height,
+                '{}'.format(height), ha=ha[xpos], va='bottom')
+
+
+autolabel(rects1, "center")
+# autolabel(rects2, "right")
+print(counted)
+plt.show()
